16503b566800209f47916e1d5522bdc1ff83908f
Santiago <Santiago603m@gmail.com>
2026-07-15 21:50:51 -0500

    v0.1_clasesPaqueteModelCompletas_andfell_15072026

---
+++ src/main/java/com/sena/parquiadero/model/Registro.java
@@ -0,0 +1,42 @@
+package com.sena.parquiadero.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "registros")
+public class Registro {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id_registro;
+	@Column(nullable = false)
+	private LocalDateTime fechaHoraIngreso;
+	private LocalDateTime fechaHorasalida;
+	private Double valorPagado;
+	@Column(nullable = false)
+	private Boolean activo = true;
+
+	@ManyToOne
+	@JoinColumn(name = "idVehiculo", nullable = false)
+	private Vehiculo vehiculo;
+
+}
---
+++ src/main/java/com/sena/parquiadero/model/Tarifa.java
@@ -0,0 +1,36 @@
+package com.sena.parquiadero.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "tarifas")
+public class Tarifa {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id_tarifa;
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private TipoVehiculo tipoVehiculo;
+	@Column(nullable = false)
+	private Double tarifaPorHora;
+	@Column(nullable = false)
+	private Double tarifaPorDia;
+}
